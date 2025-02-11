ba70b81074f7ca54fbee635a931b2cb1e856b97f
Pinformatica12023 <practicante.informatica@fla.com.co>
2025-02-11 16:49:42 -0500

    POO parte 2

--- src/library/main.go
+++ src/library/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "library/book"
+
+func main()  {
+    var myBook = book.Book{
+        Title: "Moby Dick",
+        Author: "Herman Melville",
+        Pages: 300,
+    }
+
+    myBook.printInfo()
+}
