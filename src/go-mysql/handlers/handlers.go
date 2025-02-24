2dd98ada05682f298f0d521d385777502a7510f6
Pinformatica12023 <practicante.informatica@fla.com.co>
2025-02-24 16:52:01 -0500

    Persistencia de datos con mysql, comienzo del crud

--- src/go-mysql/handlers/handlers.go
+++ src/go-mysql/handlers/handlers.go
@@ -1,6 +1,11 @@
 package handlers
 
- import "database/sql"
+ import (
+	"database/sql"
+	"log"
+	"fmt"
+	"go-mysql/models"
+)
 
  // ListContacts lista todos los contactos desde la base de datos
  func ListContacts(db *sql.DB) {
@@ -17,5 +22,97 @@ package handlers
 
 	// Iterar sobre los resultados y mostrarlos 
 	fmt.Println("\nLISTA DE CONTACTOS:")
-	fmt.Println()
+	fmt.Println("-----------------------------------------------------------------------------------")
+	for rows.Next() {
+		// Instancia del modelo contact
+		contact := models.Contact{}
+
+		var valueEmail sql.NullString 
+		err := rows.Scan(&contact.Id, &contact.Name, &valueEmail, &contact.Phone)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if valueEmail.Valid {
+			contact.Email = valueEmail.String
+		}else {
+			contact.Email = "Sin correo electrónico"
+		}
+
+		fmt.Printf("ID: %d, Nombre: %s, Email: %s, Telefono: %s\n",
+		contact.Id, contact.Name, contact.Email, contact.Phone)
+		fmt.Println("-----------------------------------------------------------------------------------")
+	}
  }
+
+ // GetContactByID obtiene un contacto de la base de datos mediante si ID
+ func GetContactByID(db *sql.DB, contactID int){
+	query := "SELECT * FROM contact WHERE id = ?"
+
+	row := db.QueryRow(query, contactID)
+
+	// Instancia de modelo de contact
+	contact := models.Contact{}
+	var valueEmail sql.NullString // Para manejar valor null. 
+
+	// Escanear el resultado en el modelo contact
+	err := row.Scan(&contact.Id, &contact.Name, &valueEmail, &contact.Phone)
+	if err != nil {
+		if err == sql.ErrNoRows{
+			log.Fatalf("No se encotró ningún contacto con el ID %d", contactID)
+		}
+	}
+
+	if valueEmail.Valid {
+		contact.Email = valueEmail.String
+	}else {
+		contact.Email = "Sin correo electrónico"
+	}
+
+	fmt.Println("\nLISTA DE CONTACTO:")
+	fmt.Println("-----------------------------------------------------------------------------------")
+	fmt.Printf("ID: %d, Nombre: %s, Email: %s, Telefono: %s\n",
+	contact.Id, contact.Name, contact.Email, contact.Phone)
+	fmt.Println("-----------------------------------------------------------------------------------")
+
+}
+
+// createContact registra un nuevo contacto en la base de datos
+func CreateContact(db *sql.DB, contact models.Contact) {
+	// Sentencia SQl para insertar un nuevo contacto
+	query := "INSERT INTO contact (name, email, phone) VALUES (?, ?, ?)"
+
+	// Ejecutar la sentencia SQL
+	_, err := db.Exec(query, contact.Name, contact.Email, contact.Phone)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Nuevo contacto registrado con éxito")
+}
+
+// UpdateContact actualiza un contacto existente en la base de datos
+func UpdateContact(db *sql.DB, contact models.Contact) {
+	query := "UPDATE contact SET name = ?, email = ?, phone = ? WHERE id = ?"
+
+	// Ejecutar la sentencia SQL
+	_, err := db.Exec(query, contact.Name, contact.Email, contact.Phone, contact.Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Contacto actualizado con éxito")
+}
+
+func DeleteContact(db *sql.DB, contactID int) {
+	// Sentencia SQL para eliminar un contacto por su ID
+	query := "DELETE FROM contact WHERE id = ?"
+
+	// Ejecutar la sentencia SQL
+	_, err := db.Exec(query, contactID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Contacto eliminado con éxito")
+}
--- src/go-mysql/main.go
+++ src/go-mysql/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-
+	"fmt"
 	"log"
 	"go-mysql/database"
 	_ "github.com/go-sql-driver/mysql"
+	"go-mysql/handlers"
+	//"go-mysql/models"
 )
 
 func main() {
@@ -15,4 +17,36 @@ func main() {
 	}
 
 	defer db.Close()
+
+
+	/*
+	// Crear una instancia de Contact
+	newContact := models.Contact{
+		Id: 3,
+		Name: "Carlo López",
+		Email: "carlosLopez@example.com",
+		Phone: "321-654-0987",
+	}
+	
+	handlers.DeleteContact(db, 4)
+
+	handlers.ListContacts(db)
+	*/
+	
+	// Que sea interactivo
+	for {
+		fmt.Println("\nMenú:")
+		fmt.Println("1. Listar contactos")
+		fmt.Println("2. Obtener contactos por ID")
+		fmt.Println("3. Crear nuevo contacto")
+		fmt.Println("4. Actualizar contacto")
+		fmt.Println("5. Eliminar contacto")
+		fmt.Println("6. Salir")
+		fmt.Println("Seleccione la opción: ")
+
+		// Leer la opción seleccionada por el usuario
+		var option int
+		fmt.Scanln(&option)
+
+	}
 }
