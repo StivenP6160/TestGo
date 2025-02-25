335aa70aadd4c3a22b5e9b26f6dd7df99c0f4ccf
Pinformatica12023 <practicante.informatica@fla.com.co>
2025-02-25 10:18:05 -0500

    Persistencia de datos con mysql, Crud finalizado con interactividad

--- src/go-mysql/main.go
+++ src/go-mysql/main.go
@@ -6,7 +6,10 @@ import (
 	"go-mysql/database"
 	_ "github.com/go-sql-driver/mysql"
 	"go-mysql/handlers"
-	//"go-mysql/models"
+	"go-mysql/models"
+	"bufio"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -18,22 +21,6 @@ func main() {
 
 	defer db.Close()
 
-
-	/*
-	// Crear una instancia de Contact
-	newContact := models.Contact{
-		Id: 3,
-		Name: "Carlo López",
-		Email: "carlosLopez@example.com",
-		Phone: "321-654-0987",
-	}
-	
-	handlers.DeleteContact(db, 4)
-
-	handlers.ListContacts(db)
-	*/
-	
-	// Que sea interactivo
 	for {
 		fmt.Println("\nMenú:")
 		fmt.Println("1. Listar contactos")
@@ -48,5 +35,62 @@ func main() {
 		var option int
 		fmt.Scanln(&option)
 
+		switch option {
+		case 1:
+			handlers.ListContacts(db)
+		case 2:
+			fmt.Print("Ingrese el ID del contacto: ")
+			var idContact int
+			fmt.Scanln(&idContact)	
+			handlers.GetContactByID(db, idContact)
+		case 3:
+			newContact := inputContactDetails(option)
+			handlers.CreateContact(db, newContact)
+			handlers.ListContacts(db)
+		case 4:
+			updateContact := inputContactDetails(option)
+			handlers.UpdateContact(db, updateContact)
+			handlers.ListContacts(db)
+		case 5:
+			fmt.Print("Ingrese el ID del contacto que quiere eliminar: ")
+			var idContact int 
+			fmt.Scanln(&idContact)
+			handlers.DeleteContact(db, idContact)
+		case 6:
+			fmt.Println("Saliendo del programa")
+			return
+		default:
+			fmt.Println("Opción no válida. Por favor, sleccione una opción válida.")
+		}
 	}
+}
+
+// Función para ingresar los detalles del contacto desde la entrada estándar
+func inputContactDetails(option int) models.Contact {
+	// Leer la entrada del usuario utilizando bufio
+	reader := bufio.NewReader(os.Stdin)
+
+	var contact models.Contact
+
+	if option == 4 {
+		fmt.Print("Ingrese el ID del contacto que quiere editar: ")
+		var idContact int
+		fmt.Scanln(&idContact)
+
+		contact.Id = idContact
+	}
+
+	fmt.Print("Ingrese el nombre del contacto: ")
+	name, _ := reader.ReadString('\n')
+	contact.Name = strings.TrimSpace(name)
+
+	fmt.Print("Ingrese el correo electrómnico del contacto: ")
+	email, _ := reader.ReadString('\n')
+	contact.Email = strings.TrimSpace(email)
+
+	fmt.Print("Ingrese el número del teléfono del contacto: ")
+	phone, _ := reader.ReadString('\n')
+	contact.Phone = strings.TrimSpace(phone)
+
+	return contact
 }
