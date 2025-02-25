07428d83ddc535e516bfee3cdb0ecc29b6fba1d7
Pinformatica12023 <practicante.informatica@fla.com.co>
2025-02-25 16:47:23 -0500

    primer commit de juego Piedra, Papel y Tijeras.

---
+++ src/rpsweb/handlers/handlers.go
@@ -0,0 +1,37 @@
+package handlers 
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+		return
+	}
+}
+
+func NewGame(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Crear nuevo juego")	
+}
+
+func Game(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Juego")	
+}
+
+func Play(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Jugar")	
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Acerca de")	
+}
