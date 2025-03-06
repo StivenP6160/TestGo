98fe7e575a640ef558c6b4d049fe5e535ecf0a05
Pinformatica12023 <practicante.informatica@fla.com.co>
2025-03-06 16:35:00 -0500

    Tercer commit juego Piedra, Papel o Tijera

--- src/rpsweb/handlers/handlers.go
+++ src/rpsweb/handlers/handlers.go
@@ -12,16 +12,40 @@ const (
 	templateBase = templateDir + "base.html"
 )
 
+type Player struct {
+	Name string
+}
+
+var player Player
+
 func Index(w http.ResponseWriter, r *http.Request) {
+	restartValue()
 	renderTemplate(w, "index.html", nil)
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
+	restartValue()
 	renderTemplate(w, "new-game.html", nil)
 }
 
 func Game(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Juego")	
+
+	if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
+		}
+
+		player.Name = r.Form.Get("name")
+	}
+
+	// Redirección a otra ruta
+	if player.Name == "" {
+		http.Redirect(w, r, "/new", http.StatusFound)
+	}
+
+	renderTemplate(w, "game.html", player)	
 }
 
 func Play(w http.ResponseWriter, r *http.Request) {
@@ -29,6 +53,7 @@ func Play(w http.ResponseWriter, r *http.Request) {
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
+	restartValue()
 	renderTemplate(w, "about.html", nil)	
 }
 
@@ -41,4 +66,9 @@ func renderTemplate(w http.ResponseWriter, page string, data any) {
 		log.Println(err)
 		return
 	}
+}
+
+// Reiniciar valores 
+func restartValue() {
+	player.Name = ""
 }
